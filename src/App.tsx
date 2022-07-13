d8425ebbc9d45ef1171fb9c50dfe5ad9f5285334
tomeryoel <96529795+tomeryoel@users.noreply.github.com>
2022-07-13 18:46:20 +0300

    The first commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>SPA- S-ingle P-age A-pplication - shopping site</h1>
+     <img src="https://thumbs.dreamstime.com/b/supermarket-cart-loaded-cardboard-boxes-sales-goods-concept-trade-commerce-online-shopping-high-delivery-order-134531493.jpg" alt=" You better buy some" />
     </div>
   );
 }
